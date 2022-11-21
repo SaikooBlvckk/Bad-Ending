818966e47f71be54c64dd8373c0c9492e670d285
SaikooBlvckk <cliner7800@gmail.com>
2022-11-21 17:44:28 -0300

    Implementación grafo

--- graph.c
+++ graph.c
@@ -1,12 +1,93 @@
+// Grafo + Lista
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <assert.h>
+#include "graph.h"
 
-typedef struct{
+struct Node{
     void *data;
     Node *up;
     Node *down;
     Node *left;
     Node *right;
-} Node;
+};
 
+struct List{
+    Node *head;
+    Node *current;
+};
+
+Node *createnode(void *data){
+    Node *new = (Node *)malloc(sizeof(Node));
+    assert(new != NULL);
+    new->data = data;
+    new->up = NULL;
+    new->down = NULL;
+    new->left = NULL;
+    new->right = NULL;
+    return new;
+}
+
+List *createList(){
+    List *l = (List *)malloc(sizeof(List *));
+    l->head = NULL;
+    l->current = l->head;
+    return l;
+}
+
+void * upList(List * list){
+  if(list->current == NULL) return NULL;
+  if (list->current->up == NULL) return NULL;
+  list->current = list->current->up;
+  return list->current->data;
+}
+
+void * downList(List * list){
+  if(list->current == NULL) return NULL;
+  if (list->current->down == NULL) return NULL;
+  list->current = list->current->down;
+  return list->current->data;
+}
+
+void * rightList(List * list){
+  if(list->current == NULL) return NULL;
+  if (list->current->right == NULL) return NULL;
+  list->current = list->current->right;
+  return list->current->data;
+}
+
+void * leftList(List * list){
+  if(list->current == NULL) return NULL;
+  if (list->current->left == NULL) return NULL;
+  list->current = list->current->left;
+  return list->current->data;
+}
+
+void pushUp(List * list, void * data){
+    Node * newNode = createNode(data);
+    newNode->down = list->current;
+    newNode->up = list->current->up;
+    list->current->up = newNode;
+}
+
+void pushDown(List * list, void * data){
+    Node * newNode = createNode(data);
+    newNode->up = list->current;
+    newNode->down = list->current->down;
+    list->current->down = newNode;
+}
+
+void pushRight(List * list, void * data){
+    Node * newNode = createNode(data);
+    newNode->left = list->current;
+    newNode->right = list->current->right;
+    list->current->right = newNode;
+}
+
+void pushLeft(List * list, void * data){
+    Node * newNode = createNode(data);
+    newNode->right = list->current;
+    newNode->left = list->current->left;
+    list->current->left = newNode;
+}
--- graph.h
+++ graph.h
@@ -1,7 +1,18 @@
 #ifndef GRAPH_h
 #define GRAPH_h
 
-typedef struct Graph Graph;
+typedef struct Node Node;
+typedef struct List List;
 
+Node *createnode(void *data);
+List *createList();
+void * upList(List * list);
+void * downList(List * list);
+void * rightList(List * list);
+void * leftList(List * list);
+void pushUp(List * list, void * data);
+void pushDown(List * list, void * data);
+void pushRight(List * list, void * data);
+void pushLeft(List * list, void * data);
 
 #endif /* GRAPH_h */
