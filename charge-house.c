6108c2ca485ebde1ee0ed48901df2bc774d4c33e
SaikooBlvckk <cliner7800@gmail.com>
2022-11-24 14:26:00 -0300

    Cambio a hashmap xd

--- charge-house.c
+++ charge-house.c
@@ -3,12 +3,17 @@
 #include <string.h>
 #include <sys/stat.h>
 #include "list.c"
-#include "graph.c"
+#include "hashmap.c"
 
 typedef struct{
   char name[30];
   List items;
-}habitacion;
+
+  char up[30];
+  char down[30];
+  char right[30];
+  char left[30];
+}Room;
 
 typedef struct{
   char name[30];
@@ -100,7 +105,11 @@ const char *get_csv_field_slash(char *tmp, int k) {
   return NULL;
 }
 
-void import_file(char *file, GraphList *Graph) {
+void createHouse(HashMap *Map, Room *room){
+  insertMap(Map, room->name, room);
+}
+
+void import_file(char *file, HashMap *Map) {
   FILE *fp = fopen(file, "r");
 
   char line[1024];
@@ -108,24 +117,26 @@ void import_file(char *file, GraphList *Graph) {
   char *aux2;
   fgets(line, 1023, fp);
   while (fgets(line, 1023, fp) != NULL) {
-    for(int i = 0; i < 7; i++){
-        int j = 0;
-        aux = (char *)malloc(sizeof(char) * 1024);
-        aux2 = (char *)malloc(sizeof(char) * 1024);
-        if (i < 5)
-            printf("%s \n", get_csv_field_coma(line, i));
-        strcpy(aux, get_csv_field_coma(line, i));
-        if(i == 5 || i == 6){
-            while (aux2 != NULL){
-                if (get_csv_field_slash(aux, j) != NULL){
-                    strcpy(aux2, get_csv_field_slash(aux, j));
-                    printf("%s ", aux2);
-                    j++;
-                }else aux2 = NULL;
-            }
-            printf("\n");
+    //int j = 0;
+    Room *room = (Room *)malloc(sizeof(Room));
+    strcpy(room->name, get_csv_field_coma(line, 0));
+    strcpy(room->up, get_csv_field_coma(line, 1));
+    strcpy(room->down, get_csv_field_coma(line, 2));
+    strcpy(room->left, get_csv_field_coma(line, 3));
+    strcpy(room->right, get_csv_field_coma(line, 4));
+
+    createHouse(Map, room);
+
+    /*if(i == 5 || i == 6){
+        while (aux2 != NULL){
+          if (get_csv_field_slash(aux, j) != NULL){
+            strcpy(aux2, get_csv_field_slash(aux, j));
+            printf("%s ", aux2);
+            j++;
+          }else aux2 = NULL;
         }
-    }
+      printf("\n");
+    }*/
   }
   printf("\n");
 }
@@ -133,10 +144,18 @@ void import_file(char *file, GraphList *Graph) {
 int main(){
   int i = 1;
   char *file;
-  GraphList *Graph = createGraphList();
+  HashMap *Map = createMap(10);
   file = (char *)malloc(sizeof(char) * 50);
 
   file = "./Casas/Casa-Dificil-1.csv";
   printf("Cargando %s\n", file);
-  import_file(file, Graph);
+  import_file(file, Map);
+
+  Pair *aux = firstMap(Map);
+  Room *room = (Room *)malloc(sizeof(Room));
+  do{
+    room = aux->value;
+    printf("%s\n arriba: %s\n abajo: %s\n izquierda: %s\n derecha: %s\n", room->name, room->up, room->down, room->left, room->right);
+    aux = nextMap(Map);
+  }while(aux != NULL);
 }
